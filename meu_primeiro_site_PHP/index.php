d0e31d3a7aec585c0db2ecdaa30dc1306de8686d
beamarques12 <117762502+beamarques12@users.noreply.github.com>
2023-08-01 16:46:47 -0300

    Add files via upload

--- meu_primeiro_site_PHP/index.php
+++ meu_primeiro_site_PHP/index.php
@@ -9,6 +9,16 @@ echo "<br>Estou concatenando" . "I love $txt!";
 $x = 10;
 $y = 10.5;
 echo "<br>" . $x + $y;
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo "<br>" . $x . "<br>";
+
+$cars = array("Volvo", "BMW", "Toyota");
+var_dump($cars);
+for ($i=0; $i<3; $i++){
+    echo "<br> carro na posição " .$i."<br>".$cars[$i];
+}
+echo "<br>". strlen("Beatriz Marques");
 ?>
 
 </body>
